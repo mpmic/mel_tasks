cb61502077384a982dbb11b0a036ab78ca6058a4
Melvin Mathew <mpmic@live.com>
2022-10-26 01:33:50 +0200

    Initial Commit

---
+++ hw01/library.h
@@ -0,0 +1,10 @@
+//
+// Created by mpmic on 25-10-2022.
+//
+
+#ifndef MEL_TASKS_LIBRARY_H
+#define MEL_TASKS_LIBRARY_H
+
+int library_function();
+
+#endif //MEL_TASKS_LIBRARY_H
--- hw01/test.cpp
+++ hw01/test.cpp
@@ -1,8 +1,8 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include <doctest/doctest.h>
+#include "doctest/doctest/doctest.h"
 
 #include "library.h"
 
 TEST_CASE("testing the library") {
-    CHECK(library_function() == 42);
+    CHECK(library_function() == 1337);
 }
