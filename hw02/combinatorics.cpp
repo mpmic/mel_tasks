b37c1bebe37beab9d586075593fc31256b3527d8
Melvin Mathew <mpmic@live.com>
2022-11-10 01:07:41 +0100

    Fix modification of test

--- hw02/combinatorics.cpp
+++ hw02/combinatorics.cpp
@@ -1,19 +1,29 @@
 #include "combinatorics.h"
 
-
 uint64_t factorial(uint64_t val) {
-    // TODO
-    return 0;
+
+    uint64_t fact = 1;
+
+    for(uint64_t count{2}; count<= val; count++){
+        fact *= count;
+    }
+
+    return fact;
 }
 
 
 uint64_t permutation(uint64_t val, uint64_t val2) {
-    // TODO
-    return 0;
+    if(val<val2)
+        return 0;
+    return factorial(val)/factorial(val-val2);
 }
 
 
 uint64_t combination(uint64_t val, uint64_t val2) {
-    // TODO
-    return 0;
+
+    if(val<val2)
+        return 0;
+
+    uint64_t comb = factorial(val)/(factorial(val2)* factorial(val-val2));
+    return comb;
 }
